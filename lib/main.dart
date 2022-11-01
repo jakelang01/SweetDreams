988306433e21332bfc8d76f8baf3d1cea3ba3fd3
lang1090 <jakelangenfeld1@gmail.com>
2022-11-01 14:19:33 -0500

    added database imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'dreams/views/dreams_component.dart';
 import 'dreams/presenter/dreams_presenter.dart';
 import 'dreams/views/dreams_input.dart';
