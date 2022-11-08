b7a0897c1ebd6e94931595eeff171275f601a8d3
lang1090 <jakelangenfeld1@gmail.com>
2022-11-08 15:04:45 -0600

    renamed things

--- lib/dreams/views/dreams_input.dart
+++ lib/dreams/views/dreams_input.dart
@@ -14,46 +14,24 @@ class SleepInput extends StatefulWidget {
 }
 
 class _SleepInputPageState extends State<SleepInput> {
-  //temp count-starting at because of sample is day 1
-  int count = 2;
-  RecordNewDay day = new RecordNewDay();
+  int count = 1;
+  RecordNewNight night = new RecordNewNight();
   @override
   void initState() {
     super.initState();
   }
 
-  final databaseReference = FirebaseFirestore.instance.collection('User 1');
-  final String addNightButton = "Add Night";
-  int nights = 1; // incrementer for nightly data
-
-  void addNight(String bedtime, String wakeUp, double sleepHours, int rating) {
-    databaseReference.doc("Night" + nights.toString()).set({"Bedtime": bedtime, "Wake Up": wakeUp, "Hours Slept": sleepHours, "Rating": rating});
-    nights++; // increments night for next call
-  }
-
-  Future<void> getNightData() async { // need to add user choosing which night's data
-    DocumentSnapshot data = await retrieveData();
-    print(data.data().toString());
-  }
-
-  Future<DocumentSnapshot> retrieveData() async {
-    return databaseReference.doc(nights.toString()).get();
-  }
-
-  void removeNight() { // need to add user choosing a specific night to delete
-    databaseReference.doc(nights.toString()).delete();
-  }
-
   //everytime function called firebase document "day" will increment by 1
-  //creaetDay is in presenter and adds data to the collection "Sleep Hours"
+  //createDay is in presenter and adds data to the collection "Sleep Hours"
   //need a button on this page so that this function can listen
   void handleNewDay(String bedtime, int quality, String wakeUp){
-    day.createDay(count, bedtime, quality, wakeUp);
+    night.createNight(count, bedtime, quality, wakeUp);
     count++;
   }
-//removes specific day from collection specific day number as parameter
+
+  //removes specific day from collection specific day number as parameter
   void deleteDay(int collectionDay){
-    day.removeDay(collectionDay);
+    night.removeNight(collectionDay);
   }
 
   @override
